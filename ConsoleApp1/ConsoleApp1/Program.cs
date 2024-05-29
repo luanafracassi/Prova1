f3d8552ab24f39dd9806e4c182b3d1f0d00c17f7
Carlo <carlosegreto95@gmail.com>
2024-05-29 12:05:47 +0200

    :P

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Prima modifica!");
+
+Console.WriteLine("Merge Conflict :P!");
+
+
 //Console.WriteLine(" Mariana modifica!");
