284fc170e37b6b9d2b951d5f9c60685965d6f752
Carlo <carlosegreto95@gmail.com>
2024-05-29 12:30:35 +0200

    deleted some console

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,2 @@
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-Console.WriteLine("Prima modifica!");
-//Console.WriteLine(" Mariana modifica!");
-Console.WriteLine("Pirponio è un bel ragazzo");
-
-Console.WriteLine("ALTRO CONFLICT PRONTO !! :P");
